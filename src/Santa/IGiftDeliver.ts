e3900d2e310bfc6a97a22473e573d4ba123756e6
Alexander <alexander.lindahl@forefront.se>
2021-09-16 13:25:33 +0200

    Remove tests and implementation code

--- src/Santa/IGiftDeliver.ts
+++
@@ -1,8 +0,0 @@
-import { House } from "House/House";
-
-export interface IGiftDeliver {
-  x: number;
-  y: number;
-  visitedHouses: Array<House>;
-  deliverGifts: (directions: Array<String>) => void;
-}
--- src/test/Elf.test.ts
+++
@@ -1,9 +0,0 @@
-import { Elf } from "../Elf/Elf";
-
-describe("Tests that the Elf works", () => {
-  it("Tests that the Elf could read directions", () => {
-    const elf = new Elf();
-
-    expect(elf.readDirections("directions.txt").length).toBe(8192);
-  });
-});
